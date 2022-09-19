5b1f06d1c990f24b5d014b598725d4d57ea8ac14
Aufritzon <w.fritzon@hotmail.se>
2022-09-19 17:01:41 +0200

    Further changes made

---
+++ FirstCSharpApp/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace RacingConsoleApp
+{
+    internal class Car
+    {
+        public string Model { get; }
+        public int HorsePower { get;}
+        public string EngineType { get; }
+        public int Speed { get; set; } = 0;
+        public Direction Dir { get; set; } = Direction.DOWN;
+        public Vector2 Pos { get; set; } = new Vector2(0, 0);
+
+        public Car(string model, int horsePower, String engineType)
+        {
+            Model = model;
+            HorsePower = horsePower;
+            EngineType = engineType;
+
+        }
+
+
+        public void Gas()
+        {
+
+        }
+
+        public void Move()
+        {
+            Pos = new Vector2(Pos.X + Speed, Pos.Y + Speed );
+
+        }
+
+        
+    }
+}
--- FirstCSharpApp/Program.cs
+++ FirstCSharpApp/Program.cs
@@ -8,45 +8,31 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 
 namespace RacingConsoleApp
 {
-
     class Program
     {
         public static void Main(string[] args)
         {
-            Car car = new Car();
+            Car car = new Car("BMWi4",45,"4 cylindrig"){Dir = Direction.UP, Speed = 45};
+            Console.WriteLine(car.Dir + car.Speed + car.EngineType + car.Model);
+            Vector2 v = new Vector2(1, 2);
+
+            v.X = 4;
+
+            car.Pos = v;
+
+
+
+            Console.WriteLine(v.X);
+
+
 
 
         }
-    }
-
-    class Car
-    {
-        public int Power { get; set; } = 4 == 4 ? 5 : 10;
-        public string EngineType { get; set; }
-
-        public Car() : this("fs") 
-        {
-            Console.WriteLine("Halleluja");
-        }
-
-
-        public Car(string engineType) : this(5, engineType) 
-        {
-            Console.WriteLine("Okay");
-        
-        }
-
-
-        public Car(int power, string engineType)
-        {
-            Power = power;
-            EngineType = engineType;
-            Console.WriteLine("normal");
-        }   
     }
 
 
