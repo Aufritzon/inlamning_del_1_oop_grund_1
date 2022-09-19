487d229290f9b7c2d6acb7bee7ac9d958cc33e35
Aufritzon <w.fritzon@hotmail.se>
2022-09-19 13:27:01 +0200

    first commit

---
+++ FirstCSharpApp/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FirstCSharpApp
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+
+
+        }
+    }
+
+    class Car
+    { 
+        public int Power { get; set; } 
+        public string EngineType { get; set; }
+
+        public Car() : this("fs") 
+        {
+            Console.WriteLine("Halleluja");
+        }
+
+
+        public Car(string engineType) : this(5, engineType) 
+        {
+            Console.WriteLine("Okay");
+        
+        }
+
+
+        public Car(int power, string engineType)
+        {
+            Power = power;
+            EngineType = engineType;
+            Console.WriteLine("normal");
+        }   
+    }
+
+
+}
